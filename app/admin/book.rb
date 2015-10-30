200ddc9a0f3af75ac60acd64078733f7f842c558
Prelang Builder <builder@prelang.com>
2015-10-30 22:45:04 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Book' to allow for create, update

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Book do
+  permit_params :title, :filepath, :added
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
