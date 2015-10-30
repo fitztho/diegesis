9b21e0f58ebf9808431c8c9e3abba299cd6c31bb
Prelang Builder <builder@prelang.com>
2015-10-30 22:44:56 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'CheckOut' to allow for create, update

--- app/admin/check_out.rb
+++ app/admin/check_out.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register CheckOut do
+  permit_params :expiration_date
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
